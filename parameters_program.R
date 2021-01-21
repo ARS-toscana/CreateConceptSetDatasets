1f5ae81ae0195e7a819d2cbf8647fda6280de804
Olga Paoletti <olga.paoletti@ars.toscana.it>
2021-01-21 12:21:00 +0100

    Upload an example of the use of the function CreateConceptSetDatasets

---
+++ parameters_program.R
@@ -0,0 +1,24 @@
+#packages
+if (!require("data.table")) install.packages("data.table")
+library(data.table)
+if (!require("stringr")) install.packages("stringr")
+library(stringr)
+
+
+diroutput <- paste0(thisdir,"/g_output/")
+dirinput <- paste0(thisdir,"/i_input/")
+extension <- c(".csv")
+
+source(paste0(thisdir,"/","CreateConceptSetDatasets.R"))
+
+###################################################################
+# CREATE FOLDERS
+###################################################################
+
+suppressWarnings(if (file.exists(diroutput)){
+  setwd(file.path(diroutput))
+} else {
+  dir.create(file.path( diroutput))
+  setwd(file.path(diroutput))
+})
+

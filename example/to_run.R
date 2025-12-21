4abddda0991524ba167bc1c48a38507c1e9a8a5c
Davide Messina <davmes@arsanita.toscana.it>
2025-12-21 18:55:18 +0100

    Added example to_run

--- example/to_run.R
+++ example/to_run.R
@@ -30,3 +30,20 @@ CreateConceptSetDatasets(concept_set_names = concept_sets_of_our_study,
                          use_qs = F,
                          add_conceptset_name=T)
                          #aggregate_concepts  #true/false o solo per concetti specifici??
+
+CreateConceptSetDatasets(concept_set_names = "spam",
+                         dataset = list(Diagnosis = list("EVENTS")),
+                         codvar = list(Diagnosis = list(EVENTS = "event_code")),
+                         datevar= list(Diagnosis = list(EVENTS = list("start_date_record", "end_date_record"))),
+                         dateformat= "YYYYmmdd",
+                         rename_col = list(person_id = list(Diagnosis = list(EVENTS = "person_id")),
+                                           date = list(Diagnosis = list(EVENTS = "start_date_record")),
+                                           main_meaning = list(EVENTS = "meaning_of_event")),
+                         concept_set_domains = list(spam = "Diagnosis"),
+                         concept_set_codes =	list(diab = list(ICD9 = c("001.0", "999.9"))),
+                         concept_set_codes_excl = list(diab = list(ICD9 = c("001.01", "999.99"))),
+                         vocabulary = list(Diagnosis = list(EVENTS = "event_record_vocabulary")),
+                         discard_from_environment = F,
+                         dirinput = dirtemp,
+                         diroutput = dirtemp,
+                         vocabularies_with_exact_search_not_dot = c("ICD9"))
